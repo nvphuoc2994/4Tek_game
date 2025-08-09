c20801775e911e8205f470cdfded3be37d57d032
phuocnguyen <nvphuoc2994@gmail.com>
2025-08-09 14:54:04 +0700

    update base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/4Tek_game/' : '/',
+  base: '/',
   plugins: [vue(), tailwindcss()],
   resolve: {
     alias: {
